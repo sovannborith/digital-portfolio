2e00aa28165b9a2efb5ab3274d54b7591effea4e
Sovannborith Yun <yun.sovannborith@gmail.com>
2022-09-30 00:43:11 +0700

    Try basedUrl and revert back

--- sanity.ts
+++ sanity.ts
@@ -2,8 +2,8 @@ import { createClient } from "next-sanity";
 import createImageUrlBuilder from "@sanity/image-url";
 
 export const config = {
-  dataset: "production", //process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-  projectId: "vqexlzak", //process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   apiVersion: "2021-10-21",
   useCdn: true,
   // useCdn: process.env.NODE_ENV === "production",
@@ -13,4 +13,10 @@ export const sanityClient = createClient(config);
 
 export const urlFor = (source: any) => {
   return createImageUrlBuilder(config).image(source);
+};
+
+export const basedUrl = () => {
+  return process.env.NODE_ENV === "production"
+    ? "https://my-digital-portfolio.vercel.app"
+    : "http://localhost:3000";
 };
