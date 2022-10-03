dadabaf706d13448c2ac149e1611112c4cfad892
Sovannborith Yun <yun.sovannborith@gmail.com>
2022-10-03 20:52:22 +0700

    Resize the skills image

--- components/SkillCardComponent.tsx
+++ components/SkillCardComponent.tsx
@@ -22,9 +22,9 @@ function SkillCardComponent({ directionLeft, skill }: Props) {
         whileInView={{ opacity: 1, x: 0 }}
         src={urlFor(skill?.image).url()}
         alt={skill?.title}
-        className="rounded-full border border-gray-500 object-cover w-16 h-16  md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
+        className="rounded-full border border-gray-500 object-cover w-16 h-16  md:w-24 md:h-24  filter group-hover:grayscale transition duration-300 ease-in-out"
       />
-      <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-16 w-16 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0">
+      <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-16 w-16 md:w-24 md:h-24 rounded-full z-0">
         <div className="flex items-center justify-center h-full">
           <p className="text-2xl font-bold text-black opacity-100">
             {skill?.progress}%
